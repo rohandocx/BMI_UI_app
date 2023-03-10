003dcbc355bb3dc9927e2b04f2cf579d71da61a2
Your Name <rohan2056.be20@chitkara.edu.in>
2023-03-10 17:20:52 +0530

    fifth Commit

---
+++ lib/reusable.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Reusable extends StatelessWidget {
+  Reusable({@required this.colour, this.cardchildcontainer, this.onpress});
+  final Color colour;
+  final Widget cardchildcontainer;
+  final Function onpress;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onpress,
+      child: Container(
+        child: cardchildcontainer,
+        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+      ),
+    );
+  }
+}
